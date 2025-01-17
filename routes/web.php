57c79a2f1a3da4bd61d9b2d01591ee5ccb23fb00
pasipasipada <irfanoputra27@gmail.com>
2025-01-17 23:28:48 +0700

    feat: add routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,60 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
+// Route halaman utama
 Route::get('/', function () {
-    return view('welcome');
+    return view('home', ['title' => 'Home Page']);
+});
+
+// Route halaman about
+Route::get('/about', function () {
+    return view('about', ['title' => 'About']);
+});
+
+// Route halaman blog
+Route::get('/blogs', function () {
+    return view('blogs', ['title' => 'Blog', 'blogs' => [
+        [
+            'id' => 1,
+            'title' => 'Judul Artikel 1',
+            'author' => 'Irfan Muria',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, repellat.'
+        ],
+        [
+            'id' => 2,
+            'title' => 'Judul Artikel 2',
+            'author' => 'Hasna Nazriah',
+            'body' => 'Love you hasna'
+        ]
+    ]]);
+});
+
+Route::get('/blog/{id}', function ($id) {
+    $blogs = [
+        [
+            'id' => 1,
+            'title' => 'Judul Artikel 1',
+            'author' => 'Irfan Muria',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, repellat.'
+        ],
+        [
+            'id' => 2,
+            'title' => 'Judul Artikel 2',
+            'author' => 'Hasna Nazriah',
+            'body' => 'Love you hasna'
+        ]
+    ];
+
+    $blog = Arr::first($blogs, function ($blog) use ($id) {
+        return $blog['id'] == $id;
+    });
+
+    return view('blog', ['title' => 'Single Blog', 'blog' => $blog]);
+});
+
+// Route halaman contact
+Route::get('/contact', function () {
+    return view('contact', ['title' => 'Contact']);
 });
