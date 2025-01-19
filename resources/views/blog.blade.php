b08bc82a022b22b5d9ac336e957ef041fd0f1d13
pasipasipada <irfanoputra27@gmail.com>
2025-01-19 22:53:47 +0700

    refactor: blog layout

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,17 +1,13 @@
 <x-layout>
     <x-slot:title>{{ $title }}</x-slot:title>
 
-    @foreach ($blogs as $blog)
-        <article class="py-8 max-w-screen-md border-b border-gray-300">
-            <a href="/blog/{{ $blog['id'] }}" class="hover:underline">
-                <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">{{ $blog['title'] }}</h2>
-            </a>
-            <div class="text-base text-gray-500">
-                <a href="#">{{ $blog['author'] }}</a> | 1 Januari 2025
-            </div>
-            <p class="my-4 font-light">{{ Str::limit($blog['body'], 100) }}</p>
-            <a href="/blog/{{ $blog['id'] }}" class="font-medium text-indigo-600 hover:underline">Read more
-                &raquo;</a>
-        </article>
-    @endforeach
+    <article class="py-8 max-w-screen-md">
+        <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">{{ $blog['title'] }}</h2>
+        <div class="text-base text-gray-500">
+            <a href="#">{{ $blog['author'] }}</a> | 1 Januari 2025
+        </div>
+        <p class="my-4 font-light">{{ $blog['body'] }}</p>
+        <a href="/blogs" class="font-medium text-indigo-600 hover:underline">&laquo; Back to blogs
+        </a>
+    </article>
 </x-layout>
