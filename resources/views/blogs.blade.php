430c3759c1658bddd328c14b535a15b26a04ed30
pasipasipada <irfanoputra27@gmail.com>
2025-01-19 22:56:37 +0700

    feat: update blogs URLs to use slug instead of ID

--- resources/views/blogs.blade.php
+++ resources/views/blogs.blade.php
@@ -3,14 +3,14 @@
 
     @foreach ($blogs as $blog)
         <article class="py-8 max-w-screen-md border-b border-gray-300">
-            <a href="/blogs/{{ $blog['id'] }}" class="hover:underline">
+            <a href="/blogs/{{ $blog['slug'] }}" class="hover:underline">
                 <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">{{ $blog['title'] }}</h2>
             </a>
             <div class="text-base text-gray-500">
                 <a href="#">{{ $blog['author'] }}</a> | 1 Januari 2025
             </div>
             <p class="my-4 font-light">{{ Str::limit($blog['body'], 100) }}</p>
-            <a href="/blogs/{{ $blog['id'] }}" class="font-medium text-indigo-600 hover:underline">Read more
+            <a href="/blogs/{{ $blog['slug'] }}" class="font-medium text-indigo-600 hover:underline">Read more
                 &raquo;</a>
         </article>
     @endforeach
