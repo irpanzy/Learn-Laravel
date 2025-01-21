6414636e7b844d5a291798288dfbfc7bf22e646b
pasipasipada <irfanoputra27@gmail.com>
2025-01-21 22:18:12 +0700

    refactor: update navigation layout and styling

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,17 +1,20 @@
-<nav class="bg-gray-800" x-data="{ isOpen: false }">
+<nav class="bg-blue-200" x-data="{ isOpen: false }">
     <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div class="flex h-16 items-center justify-between">
             <div class="flex items-center">
                 <div class="shrink-0">
-                    <img class="size-8" src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=500"
-                        alt="Muria Cell">
+                    <img class="size-8" src="{{ asset('img/muria-cell.png') }}" alt="Logo Image">
                 </div>
                 <div class="hidden md:block">
                     <div class="ml-10 flex items-baseline space-x-4">
-                        <x-nav-link href="/" :active="request()->is('/')">Home</x-nav-link>
-                        <x-nav-link href="/blogs" :active="request()->is('blogs')">Blog</x-nav-link>
-                        <x-nav-link href="/about" :active="request()->is('about')">About</x-nav-link>
-                        <x-nav-link href="/contact" :active="request()->is('contact')">Contact</x-nav-link>
+                        <x-nav-link href="/" :active="request()->is('/')"
+                            class="text-gray-800 hover:bg-blue-800 hover:text-white rounded-md px-3 py-2 text-base">Home</x-nav-link>
+                        <x-nav-link href="/blogs" :active="request()->is('blogs')"
+                            class="text-gray-800 hover:bg-blue-800 hover:text-white rounded-md px-3 py-2 text-base">Blog</x-nav-link>
+                        <x-nav-link href="/about" :active="request()->is('about')"
+                            class="text-gray-800 hover:bg-blue-800 hover:text-white rounded-md px-3 py-2 text-base">About</x-nav-link>
+                        <x-nav-link href="/contact" :active="request()->is('contact')"
+                            class="text-gray-800 hover:bg-blue-800 hover:text-white rounded-md px-3 py-2 text-base">Contact</x-nav-link>
                     </div>
                 </div>
             </div>
@@ -21,13 +24,12 @@
                     <div class="relative ml-3">
                         <div>
                             <button type="button" @click="isOpen = !isOpen"
-                                class="relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                                class="relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-blue-200"
                                 id="user-menu-button" aria-expanded="false" aria-haspopup="true">
                                 <span class="absolute -inset-1.5"></span>
                                 <span class="sr-only">Open user menu</span>
-                                <img class="size-8 rounded-full"
-                                    src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                                    alt="">
+                                <img class="size-11 rounded-full" src="{{ asset('img/profile-irpan.jpeg') }}"
+                                    alt="Profile Image">
                             </button>
                         </div>
                         <div x-show="isOpen" x-transition:enter="transition ease-out duration-100 transform"
@@ -37,11 +39,11 @@
                             class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black/5 focus:outline-none"
                             role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button"
                             tabindex="-1">
-                            <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem"
+                            <a href="#" class="block px-4 py-2 text-sm text-gray-800" role="menuitem"
                                 tabindex="-1" id="user-menu-item-0">Your Profile</a>
-                            <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem"
+                            <a href="#" class="block px-4 py-2 text-sm text-gray-800" role="menuitem"
                                 tabindex="-1" id="user-menu-item-1">Settings</a>
-                            <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem"
+                            <a href="#" class="block px-4 py-2 text-sm text-gray-800" role="menuitem"
                                 tabindex="-1" id="user-menu-item-2">Sign out</a>
                         </div>
                     </div>
@@ -50,7 +52,7 @@
             <div class="-mr-2 flex md:hidden">
                 <!-- Mobile menu button -->
                 <button type="button" @click="isOpen = !isOpen"
-                    class="relative inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                    class="relative inline-flex items-center justify-center rounded-md p-2 text-black-400 hover:bg-blue-800 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-blue-200"
                     aria-controls="mobile-menu" aria-expanded="false">
                     <span class="absolute -inset-0.5"></span>
                     <span class="sr-only">Open main menu</span>
@@ -75,13 +77,16 @@
     <!-- Mobile menu, show/hide based on menu state. -->
     <div x-show="isOpen" class="md:hidden" id="mobile-menu">
         <div class="space-y-1 px-2 pb-3 pt-2 sm:px-3">
-            <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-            <x-nav-link href="/" :active="request()->is('/')">Home</x-nav-link>
-            <x-nav-link href="/blogs" :active="request()->is('blogs')">Blog</x-nav-link>
-            <x-nav-link href="/about" :active="request()->is('about')">About</x-nav-link>
-            <x-nav-link href="/contact" :active="request()->is('contact')">Contact</x-nav-link>
+            <x-nav-link href="/" :active="request()->is('/')"
+                class="text-gray-800 hover:bg-blue-800 hover:text-white rounded-md px-3 py-2 text-base">Home</x-nav-link>
+            <x-nav-link href="/blogs" :active="request()->is('blogs')"
+                class="text-gray-800 hover:bg-blue-800 hover:text-white rounded-md px-3 py-2 text-base">Blog</x-nav-link>
+            <x-nav-link href="/about" :active="request()->is('about')"
+                class="text-gray-800 hover:bg-blue-800 hover:text-white rounded-md px-3 py-2 text-base">About</x-nav-link>
+            <x-nav-link href="/contact" :active="request()->is('contact')"
+                class="text-gray-800 hover:bg-blue-800 hover:text-white rounded-md px-3 py-2 text-base">Contact</x-nav-link>
         </div>
-        <div class="border-t border-gray-700 pb-3 pt-4">
+        <div class="border-t border-gray=-700 pb-3 pt-4">
             <div class="flex items-center px-5">
                 <div class="shrink-0">
                     <img class="size-10 rounded-full"
@@ -89,18 +94,18 @@
                         alt="">
                 </div>
                 <div class="ml-3">
-                    <div class="text-base/5 font-medium text-white">Tom Cook</div>
-                    <div class="text-sm font-medium text-gray-400">tom@example.com</div>
+                    <div class="text-base/5 font-medium text-gray-800">Tom Cook</div>
+                    <div class="text-sm font-medium text-gray-800">tom@example.com</div>
                 </div>
             </div>
             <div class="mt-3 space-y-1 px-2">
                 <a href="#"
-                    class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white">Your
+                    class="block rounded-md px-3 py-2 text-base font-medium text-gray-800 hover:bg-blue-800 hover:text-white">Your
                     Profile</a>
                 <a href="#"
-                    class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white">Settings</a>
+                    class="block rounded-md px-3 py-2 text-base font-medium text-gray-800 hover:bg-blue-800 hover:text-white">Settings</a>
                 <a href="#"
-                    class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white">Sign
+                    class="block rounded-md px-3 py-2 text-base font-medium text-gray-800 hover:bg-blue-800 hover:text-white">Sign
                     out</a>
             </div>
         </div>
