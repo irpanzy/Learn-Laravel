ffc4cfce13aa153728d9402c741a8a6bad322c87
pasipasipada <irfanoputra27@gmail.com>
2025-01-21 22:20:03 +0700

    feat: add new models and migrations for blogs

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -3,37 +3,6 @@
 namespace App\Models;
 
 use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Model;
 
-class Blog
-{
-    public static function all()
-    {
-        return [
-            [
-                'id' => 1,
-                'slug' => 'judul-artikel-1',
-                'title' => 'Judul Artikel 1',
-                'author' => 'Irfan Muria',
-                'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, repellat.'
-            ],
-            [
-                'id' => 2,
-                'slug' => 'judul-artikel-2',
-                'title' => 'Judul Artikel 2',
-                'author' => 'Firza Rasyid Muria',
-                'body' => 'Halo Zaza'
-            ]
-        ];
-    }
-
-    public static function find($slug): array
-    {
-        $blog = Arr::first(static::all(), fn($blog) => $blog['slug'] == $slug);
-
-        if (!$blog) {
-            abort(404);
-        }
-
-        return $blog;
-    }
-}
+class Blog extends Model {}
